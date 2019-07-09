2b28f99d8b490b7c425535f1fe90146abc99c4ac
Nam Thai Hoang <namthaih@usc.edu>
2019-07-09 14:04:59 +0900

    Add build to deploy on elastic beanstalk

--- index.js
+++ index.js
@@ -38,5 +38,17 @@ universityRoutes(app);
 highSchoolRoutes(app);
 cityRoutes(app);
 
+if (process.env.NODE_ENV === "production") {
+	//Express will serve up production assets like our main.js file
+	//if get requests come in and we don't understand what it is, look in "client/build" to see if a file there matches
+	app.use(express.static("client/build"));
+
+	//Express will serve up the index.htm file if it doesn't recognize the route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
